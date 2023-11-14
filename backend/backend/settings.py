76fe6cf5b30c17293fc6a107717683b56bef646a
Brendo-Costa <aldenir.brendo@gmail.com>
2023-11-14 18:04:39 -0300

    feat: alteração deploy.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,16 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-""" # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)uz_hs!yvd+c0_20b3*qb97farbsqviz(qze1u)*bm%c+&_l+'
-
-# SECURITY WARNING: don't run with debug turned on in production! (Deve ser "False" em PRODUÇÃO)
-DEBUG = True
-
-# QUAIS DOMÍNIOS/IP'S PODEM ACESSAR A SUA APLICAÇÃO DJANGO, NO EXEMPLO, ESTÁ O IP DO SERVIDOR.
-ALLOWED_HOSTS = ['*'] """
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -71,18 +61,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-
-
-""" # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -143,7 +121,6 @@ STATIC_ROOT = BASE_DIR / 'static'  # collectstatic
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
 # settings.py
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
@@ -152,6 +129,26 @@ REST_FRAMEWORK = {
     ),
 }
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3)uz_hs!yvd+c0_20b3*qb97farbsqviz(qze1u)*bm%c+&_l+'
+
+# SECURITY WARNING: don't run with debug turned on in production! (Deve ser "False" em PRODUÇÃO)
+DEBUG = True 
+
+# QUAIS DOMÍNIOS/IP'S PODEM ACESSAR A SUA APLICAÇÃO DJANGO, NO EXEMPLO, ESTÁ O IP DO SERVIDOR.
+ALLOWED_HOSTS = ['34.125.36.174']
+
+# DATABASE CONFIGURATIONS
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'api_db',
+        'USER': 'usuario_api',
+        'PASSWORD': 'capacete1040.',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 try:
     from backend.backend.local_settings import *
